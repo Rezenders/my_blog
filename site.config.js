156a3051764b27e88e3792f74256395ff8d7e11b
Gustavo <g.rezendesilva@tudelft.nl>
2023-04-23 13:33:32 +0200

    🔧 adjusting config

--- site.config.js
+++ site.config.js
@@ -1,13 +1,14 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Gustavo Rezende",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "",
+    bio: "",
+    email: "gustavorezendesilva@hotmail.com",
+    linkedin: "rezenders",
+    github: "rezenders",
+    twitter: "grezenders",
     instagram: "",
   },
   projects: [
@@ -18,14 +19,14 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Rezenders world",
+    description: "Welcome!",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "https://rezenders-world.vercel.app",
+  since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
@@ -53,7 +54,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: "morethanmin/morethan-log",
+      repo: "rezenders/my_blog",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
